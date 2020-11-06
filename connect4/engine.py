26a60c55422d8127e3a8bb0ae892489c7cce50ae
arthur.vella@ynov.com <arthur.vella@ynov.com>
2020-11-06 13:48:00 +0100

    AI working

--- connect4/engine.py
+++ connect4/engine.py
@@ -1,25 +1,30 @@
+from colorama import init, Fore, Style
+from solver import bestMove
+import sys
+
+
 class Engine:
     def __init__(self):
 
         self.board_size = 7
         self.board = [[None for _ in range(7)] for _ in range(7)]
-        self.round = "X"
+        self.round = 1
 
     def printBoard(self):
-        print("┌" + "─" * self.board_size + "┐")
+        print("┌" + "─" * self.board_size * 2 + "─┐")
         for j in range(len(self.board)):
             print("│", end="")
             for i in range(len(self.board[j])):
                 if self.board[i][j] == None:
-                    print(" ", end="")
-                elif self.board[i][j] == "X":
-                    print("X", end="")
-                elif self.board[i][j] == "O":
-                    print("O", end="")
-            print("│\n", end="")
+                    print("  ", end="")
+                elif self.board[i][j] == 1:
+                    print(Fore.RED + " ●" + Style.RESET_ALL, end="")
+                elif self.board[i][j] == 2:
+                    print(Fore.YELLOW + " ●" + Style.RESET_ALL, end="")
+            print(" │\n", end="")
 
-        print("└" + "─" * self.board_size + "┘")
-        print(" " + "".join(str(i) for i in range(1, self.board_size + 1)) + " ")
+        print("└" + "─" * self.board_size * 2 + "─┘")
+        print("  " + " ".join(str(i) for i in range(1, self.board_size + 1)) + "  ")
 
     def isBoardFull(self):
         for j in range(len(self.board)):
@@ -35,20 +40,20 @@ class Engine:
             return False
         if col < 1 or col > 7:
             return False
-        row = 0
-        for i in range(1, self.board_size):
+        row = -1
+        for i in range(self.board_size):
             if self.board[col - 1][i] != None:
                 break
             row += 1
-        if row > 7:
+        if row == -1:
             return False
         self.board[col - 1][row] = self.round
-        self.round = "X" if self.round == "O" else "O"
+        self.round = 1 if self.round == 2 else 2
         return True
 
     def isWinner(self):
 
-        for player in ["X", "O"]:
+        for player in [1, 2]:
             # Horizontal check
             for j in range(len(self.board)):
                 for i in range(len(self.board[j]) - 3):
@@ -97,21 +102,32 @@ class Engine:
 
 
 if __name__ == "__main__":
+    init()
     engine = Engine()
+
+    mode = sys.argv[1]
+
     while not engine.isBoardFull():
         engine.printBoard()
-        playerinput = False
-        while not playerinput:
-            print("TURN: Player 1" if engine.round == "X" else "TURN: Player 2")
-            playerinput = engine.play(input("Choose column to play (1 to 7) : "))
+
+        if engine.round == 1 or mode == "2P":
+            playerinput = False
+            while not playerinput:
+                print("TURN: Player 1" if engine.round == 1 else "TURN: Player 2")
+                playerinput = engine.play(input("Choose column to play (1 to 7) : "))
+        else:
+            difficulty = "medium"
+            if mode in ["easy", "medium", "hard", "impossible"]:
+                difficulty = mode
+            bestCol = bestMove(engine.board, 2, difficulty)
+            engine.play(bestCol)
 
         w = engine.isWinner()
         if w:
             engine.printBoard()
-            print("Player 1 wins !" if w == "X" else "Player 2 wins !")
+            print("Player 1 wins !" if w == 1 else "Player 2 wins !")
             break
 
     if not w:
         engine.printBoard()
         print("Tie")
-
--- connect4/solver.py
+++ connect4/solver.py
@@ -1,12 +1,180 @@
 import math
-
-player_1 = "X"
-player_2 = "O"
+import random
 
 
-def bestMove(board, player):
-    pass
+def bestMove(board, player, difficulty):
+    difficulties = {"easy": 1, "medium": 2, "hard": 3, "impossible": 5}
+    return (
+        _minmaxAlgorithm(
+            board, difficulties[difficulty], -math.inf, math.inf, True, player
+        )[0]
+        + 1
+    )
 
 
-def _minmaxAlgorithm(board, depth, alpha, beta, playerTurn):
-    pass
+def _minmaxAlgorithm(board, depth, alpha, beta, playerTurn, player):
+    opponent = 1 if player == 2 else 2
+    validSpots = _findSpots(board)
+
+    if depth == 0:
+        return None, _boardScore(board, 2)
+    if _isWinner(board, player):
+        return None, 100000000000000
+    if _isWinner(board, opponent):
+        return None, -100000000000000
+    if len(validSpots) == 0:
+        return None, 0
+
+    if playerTurn:
+        value = -math.inf
+        bestMove, _ = random.choice(validSpots)
+        for col, row in validSpots:
+            copyBoard = _copyOfBoard(board)
+            copyBoard[col][row] = player
+            _, new_score = _minmaxAlgorithm(
+                copyBoard, depth - 1, alpha, beta, False, player
+            )
+            if new_score > value:
+                value = new_score
+                bestMove = col
+            alpha = max(alpha, value)
+            if alpha >= beta:
+                break
+        return bestMove, value
+    else:
+        value = math.inf
+        bestMove, _ = random.choice(validSpots)
+        for col, row in validSpots:
+            copyBoard = _copyOfBoard(board)
+            copyBoard[col][row] = opponent
+            _, new_score = _minmaxAlgorithm(
+                copyBoard, depth - 1, alpha, beta, True, player
+            )
+            if new_score < value:
+                value = new_score
+                bestMove = col
+            beta = min(beta, value)
+            if alpha >= beta:
+                break
+        return bestMove, value
+
+
+def _isWinner(board, player):
+    # Horizontal check
+    for j in range(len(board)):
+        for i in range(len(board[j]) - 3):
+            if (
+                board[i][j] == player
+                and board[i + 1][j] == player
+                and board[i + 2][j] == player
+                and board[i + 3][j] == player
+            ):
+                return player
+
+    # Vertical check
+    for j in range(len(board) - 3):
+        for i in range(len(board[j])):
+            if (
+                board[i][j] == player
+                and board[i][j + 1] == player
+                and board[i][j + 2] == player
+                and board[i][j + 3] == player
+            ):
+                return player
+
+    # Up Left – Bottom Right Diagonal check
+    for j in range(len(board) - 3):
+        for i in range(len(board[j]) - 3):
+            if (
+                board[i][j] == player
+                and board[i + 1][j + 1] == player
+                and board[i + 2][j + 2] == player
+                and board[i + 3][j + 3] == player
+            ):
+                return player
+
+    # Bottom left – Up Right Diagonal check
+    for j in range(3, len(board)):
+        for i in range(len(board[j]) - 3):
+            if (
+                board[i][j] == player
+                and board[i + 1][j - 1] == player
+                and board[i + 2][j - 2] == player
+                and board[i + 3][j - 3] == player
+            ):
+                return player
+
+
+def _findSpots(board):
+    spots = []
+    for col in range(len(board)):
+        if board[col][0] == None:
+            for row in range(len(board[col])):
+                if board[col][row] != None:
+                    spots.append((col, row - 1))
+                    break
+                if row == len(board[col]) - 1:
+                    spots.append((col, row))
+                    break
+    return spots
+
+
+def _boardScore(board, player):
+    score = 0
+
+    # Center column
+    centerColumn = [board[len(board) // 2][i] for i in range(len(board))]
+    score += centerColumn.count(player) * 3
+
+    # Vertical
+    for col in range(len(board)):
+        fullColumn = [board[col][i] for i in range(len(board))]
+        for row in range(len(board[col]) - 3):
+            group4pieces = fullColumn[row : row + 4]
+            score += _scoreFrom4Pieces(group4pieces, player)
+
+    # Horizontal
+    for row in range(len(board)):
+        fullRow = [board[i][row] for i in range(len(board))]
+        for col in range(len(board[row]) - 3):
+            group4pieces = fullRow[row : row + 4]
+            score += _scoreFrom4Pieces(group4pieces, player)
+
+    # Up Left – Bottom Right Diagonal check
+    for col in range(len(board) - 3):
+        for row in range(len(board[col]) - 3):
+            group4pieces = [board[col + i][row + i] for i in range(4)]
+            score += _scoreFrom4Pieces(group4pieces, player)
+
+    # Bottom left – Up Right Diagonal check
+    for col in range(len(board) - 3):
+        for row in range(len(board[col]) - 3):
+            group4pieces = [board[col + 3 - i][row + i] for i in range(4)]
+            score += _scoreFrom4Pieces(group4pieces, player)
+
+    return score
+
+
+def _scoreFrom4Pieces(group, player):
+    score = 0
+    opponent = 2 if player == 1 else 1
+
+    if group.count(player) == 4:
+        score += 100
+    elif group.count(player) == 3 and group.count(None) == 1:
+        score += 5
+    elif group.count(player) == 2 and group.count(None) == 2:
+        score += 2
+
+    if group.count(opponent) == 3 and group.count(None) == 1:
+        score -= 4
+
+    return score
+
+
+def _copyOfBoard(board):
+    copyB = [[None for _ in range(7)] for _ in range(7)]
+    for c in range(len(board)):
+        for r in range(len(board[c])):
+            copyB[c][r] = board[c][r]
+    return copyB
