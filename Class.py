73f7088f7f1d0cc7d1a33616f65b933ccd103da2
Matteo-Ynov <matteo.christophe@ynov.com>
2020-11-18 13:23:49 +0100

    Graphical game + updated AI

--- Class.py
+++ Class.py
@@ -19,6 +19,7 @@ class Player:
         self.attack_board = Board()
         self.random_placement(self.boat)
         self.destroyed_boats = 0
+        self.depth = 5000
 
     def random_placement(self, boats):
         for boat_value, boat_length in boats:
@@ -48,25 +49,23 @@ class Player:
 
     def SendHit(self, x, y, Player):
         if self.validHit(x, y):
-            try:
-                id_boat = int(Player.defense_board.board[x][y])
-            except:
-                id_boat = int(Player.Player.defense_board.board[x][y])
+            id_boat = int(Player.defense_board.board[x][y])
             if id_boat == 0:
                 self.attack_board.board[x][y] = 6
-                print(f"Plouf en {x+1},{y+1}")
+                Player.defense_board.board[x][y] = 6
             else:
-                print(f"Touché en {x+1},{y+1}")
+                print(f"✓✓✓ Touché en {x+1},{y+1} ✓✓✓")
+                Player.defense_board.board[x][y] = 7
                 self.attack_board.board[x][y] = id_boat
                 self.destroyed[id_boat-1][1] += 1
                 if self.CheckDestroyed(id_boat-1):
                     self.Destroy(id_boat)
                     self.destroyed_boats += 1
                     if self.destroyed_boats == 5:
-                        return True
-            return False
+                        return (True, True)
+            return (False, True)
         else:
-            return False
+            return (False, False)
 
     def validHit(self, x, y):
         if 0 <= x <= 9 and 0 <= y <= 9:
@@ -84,6 +83,121 @@ class Player:
             for j in range(10):
                 if int(self.attack_board.board[i][j]) == boat:
                     self.attack_board.board[i][j] = 7
+
+
+    def seach_best_move(self):
+        self.dico = {}
+        list_boat = []
+        for boat in self.destroyed:
+            if not self.CheckDestroyed(boat[0] - 1):
+                list_boat.append(self.boat[boat[0] - 1])
+        for i in range(self.depth):
+            for boat in list_boat:
+                self.hunt(boat[1])
+        self.destroy()
+        print(self.attack_board.board)
+        print(self.heatmap())
+        coo = list(self.dico.keys())[list(self.dico.values()).index(max(self.dico.values()))]
+        if coo[0] < 0 or coo[1] < 0:
+            del self.dico[coo]
+        return(list(self.dico.keys())[list(self.dico.values()).index(max(self.dico.values()))])
+
+
+    def hunt(self, boat):
+        placed = False
+        board = self.attack_board.get_board(copy = True)
+        tries = 0
+        while placed == False:
+            if tries == self.depth/10:
+                placed = True
+            vertical = randint(0,1)
+            if vertical:
+                x, y = randint(0,9 - boat + 1), randint(0,9)
+                valid = True
+                for i in range(x, x + boat):
+                    if board[i][y] == 6:
+                        valid = False
+                if valid == True:
+                    for i in range(x, x + boat):
+                        if (i % 2 == 0 and y % 2 == 1) or (i % 2 == 1 and y % 2 == 0):
+                            if (i, y) in self.dico:
+                                self.dico[(i,y)] += 1
+                            else:
+                                if board[i][y] == 0:
+                                    self.dico[(i,y)] = 1
+                        placed = True
+            else:
+                x, y = randint(0,9), randint(0,9 - boat + 1)
+                valid = True
+                for i in range(y, y + boat):
+                    if board[x][i] == 11:
+                        valid = False
+                if valid == True:
+                    for i in range(y, y + boat):
+                        if (i % 2 == 0 and x % 2 == 1) or (i % 2 == 1 and x % 2 == 0):
+                            if (x, i) in self.dico:
+                                self.dico[(x,i)] += 1
+                            else:
+                                if board[x][i] == 0:
+                                    self.dico[(x,i)] = 1
+                        placed = True
+            tries += 1
+
+    def destroy(self):
+        board = self.attack_board.board
+        for i in range(10):
+            for j in range(10):
+                neighbours = []
+                if int(board[i][j]) != 0 and int(board[i][j]) != 6 and int(board[i][j]) != 7:
+                    for adj_x, adj_y in [(i-1, j), (i+1, j), (i, j-1), (i, j+1)]:
+                        try:
+                            if board[adj_x][adj_y] == board[i][j]:
+                                neighbours.append((adj_x, adj_y))
+                        except:
+                            pass
+                    if len(neighbours) == 0:
+                        for adj_x, adj_y in [(i-1, j), (i+1, j), (i, j-1), (i, j+1)]:
+                            try:
+                                if int(board[adj_x][adj_y]) == 0:
+                                    if (adj_x, adj_y) in self.dico:
+                                        self.dico[(adj_x, adj_y)] += 10000
+                                    else:
+                                        self.dico[(adj_x, adj_y)] = 10000
+                            except:
+                                pass
+                    else:
+                        for x, y in neighbours:
+                            xaxys = x - i
+                            yaxys = y - j
+                            if xaxys != 0:
+                                try:
+                                    if int(board[i-xaxys][j]) == 0:
+                                        if (i-xaxys, j) in self.dico:
+                                            self.dico[(i-xaxys, j)] += 10000
+                                        else:
+                                            self.dico[(i-xaxys, j)] = 10000
+                                except:
+                                    pass
+                            elif yaxys != 0:
+                                try:
+                                    if int(board[i][j-yaxys]) == 0:
+                                        if (i, j-yaxys) in self.dico:
+                                            self.dico[(i, j-yaxys)] += 10000
+                                        else:
+                                            self.dico[(i, j-yaxys)] = 10000
+                                except:
+                                    pass
+
+
+    def heatmap(self):
+        heatmap = np.zeros((10, 10))
+        for i in range(10):
+            for j in range(10):
+                try:
+                    heatmap[i][j] = self.dico[(i,j)] / 1000
+                except:
+                    heatmap[i][j] = 0
+        return(heatmap)
                 
 
         
--- bataillenavale.py
+++ bataillenavale.py
@@ -1,18 +1,76 @@
 from Class import Board, Player
-from IA import IA
 from random import randint
+import pygame
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+WIDTH = 60
+HEIGHT = 60
+MARGIN = 15
+
+pygame.init()
+win = pygame.display.set_mode((1600, 765))
+pygame.display.set_caption("Bataille navale")
+clock = pygame.time.Clock()
 
 player = Player()
-bot = IA(Player())
+bot = Player()
 finished = False
-while not(finished):
-    print("======Player Turn======")
-    player.SendHit(int(input("x ? "))-1, int(input("y ? "))-1, bot)
-    print(player.attack_board.board)
-    print("\n\n======Bot Turn======")
-    coo = bot.seach_best_move()
-    win = bot.Player.SendHit(coo[0], coo[1], player)
-    print(bot.Player.attack_board.board)
-    if win:
-        finished = True
+running = True
+bot_turn = False
+while running:
+    clock.tick(60)
+    pygame.display.update()
+    
+    if bot_turn:
+        coo = bot.seach_best_move()
+        output = bot.SendHit(coo[0], coo[1], player)
+        if output[0]:
+            running = False
+            winner = "Bot"
+        bot_turn = False
+        
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            pos = pygame.mouse.get_pos()
+            column = pos[0] // (WIDTH + MARGIN) - 11
+            row = pos[1] // (HEIGHT + MARGIN)
+            if not(bot_turn):
+                output = player.SendHit(row, column, bot)
+                print(output)
+                if output[0]:
+                    running = False
+                    winner = "Player"
+                if output[1]:
+                    bot_turn = True
+    win.fill(WHITE)
+    for index, board in enumerate([player.attack_board.board, player.defense_board.board]):
+        for row, i in enumerate(board):
+            for column, case in enumerate(i):
+                offset = 0
+                if index == 0:
+                    offset = 835
+                if case == 0:
+                    color = BLUE
+                elif case == 6:
+                    color = BLACK
+                elif case == 7:
+                    color = RED
+                else:
+                    color = GREEN
+                pygame.draw.rect(win,
+                                 color,
+                                 [(MARGIN + WIDTH) * column + MARGIN + offset,
+                                  (MARGIN + HEIGHT) * row + MARGIN,
+                                  WIDTH,
+                                  HEIGHT])
 
+
+    pygame.display.flip()
+print(f"{winner} a gagné")
+pygame.quit()
