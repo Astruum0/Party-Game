4ab74a389df1eb632f14b13b3377164e301d9928
Matteo-Ynov <matteo.christophe@ynov.com>
2020-11-25 10:55:27 +0100

    Added Heatmap creation

--- BattleShip.py
+++ BattleShip.py
@@ -2,15 +2,18 @@ from Class import Board, Player
 from random import randint
 import pygame
 import sys
+import imageio
 
+# Get arguments for difficulty
 try:
     difficulty = sys.argv[1]
-    if difficulty not in(["easy","medium","hard","impossible"]): 
+    if difficulty not in(["easy","medium","hard"]): 
         raise TypeError
 except:
-    print("Argument non valide, difficulté mise a normal")
+    print("Argument non valide, difficulté mise a normal, lancez BattleShip.py [easy, medium, hard] pour choisir votre difficulté")
     difficulty = "medium"
 
+# Define colors and dimensions
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
@@ -25,15 +28,20 @@ win = pygame.display.set_mode((1600, 765))
 pygame.display.set_caption("Bataille navale")
 clock = pygame.time.Clock()
 
+# Create instance for the player and the AI
 player = Player(None)
 bot = Player(difficulty)
+
 finished = False
 running = True
-bot_turn = False
+bot_turn = randint(0,1)
+coup = 0
+# Game Loop
 while running:
     clock.tick(60)
     pygame.display.update()
     
+    # Player and AI turn
     if bot_turn:
         coo = bot.seach_best_move()
         output = bot.SendHit(coo[0], coo[1], player)
@@ -42,6 +50,7 @@ while running:
             winner = "bot"
         if not(output[0]) and not(output[1]):
             bot_turn = False
+        coup += 1
         
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -57,6 +66,8 @@ while running:
                     winner = "Player"
                 if not(output[0]) and not(output[1]):
                     bot_turn = True
+
+    # Draw game
     win.fill(WHITE)
     for index, board in enumerate([player.attack_board.board, player.defense_board.board]):
         for row, i in enumerate(board):
@@ -68,7 +79,7 @@ while running:
                     color = BLUE
                 elif case == 6:
                     color = BLACK
-                elif case == 7:
+                elif case == 12:
                     color = RED
                 else:
                     color = GREEN
@@ -82,4 +93,6 @@ while running:
 
     pygame.display.flip()
 print(f"{winner} a gagné")
+imageio.mimsave('heatmap/heatmap.gif', bot.images, fps = 2)
+print(coup)
 pygame.quit()
