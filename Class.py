a9727500c6f49cf52b695ed18a928a58aecedc55
Matteo-Ynov <matteo.christophe@ynov.com>
2020-12-09 09:34:04 +0100

    Des trucs

--- Class.py
+++ Class.py
@@ -231,7 +231,7 @@ class Player:
 
     # Heatmap gif creator
     def heatmap(self):
-        if self.difficulty == "medium":
+        if self.difficulty == "hard":
             heatmap = np.zeros((10, 10))
             for i in range(10):
                 for j in range(10):
