216ac0e74b2d957713f07bf9c0b994c9e8625f06
Matteo-Ynov <matteo.christophe@ynov.com>
2021-02-09 20:50:03 +0100

    Changed difficulty

--- BattleShip.py
+++ BattleShip.py
@@ -3,15 +3,20 @@ from random import randint
 import pygame
 import sys
 import imageio
+from start import place_boat
 
 # Get arguments for difficulty
 try:
     difficulty = sys.argv[1]
-    if difficulty not in(["easy","medium","hard"]): 
+    if difficulty not in (["easy", "medium", "hard"]):
         raise TypeError
 except:
-    print("Argument non valide, difficulté mise a normal, lancez BattleShip.py [easy, medium, hard] pour choisir votre difficulté")
+    print(
+        "Argument non valide, difficulté mise a normal, lancez Battleship.py [easy, medium, hard] pour choisir votre difficulté"
+    )
     difficulty = "medium"
+# Launch first screen to place the boat
+board = list(place_boat())
 
 # Define colors and dimensions
 BLACK = (0, 0, 0)
@@ -29,19 +34,18 @@ pygame.display.set_caption("Bataille navale")
 clock = pygame.time.Clock()
 
 # Create instance for the player and the AI
-player = Player(None)
-print(player.defense_board.board)
-bot = Player(difficulty)
+player = Player(None, board)
+bot = Player(difficulty, None)
 
 finished = False
 running = True
-bot_turn = randint(0,1)
+bot_turn = randint(0, 1)
 coup = 0
 # Game Loop
 while running:
     clock.tick(60)
     pygame.display.update()
-    
+
     # Player and AI turn
     if bot_turn:
         coo = bot.search_best_move()
@@ -49,10 +53,9 @@ while running:
         if output[2]:
             running = False
             winner = "bot"
-        if not(output[0]) and not(output[1]):
+        if output[0] and not (output[1]):
             bot_turn = False
         coup += 1
-        
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -60,17 +63,18 @@ while running:
             pos = pygame.mouse.get_pos()
             column = pos[0] // (WIDTH + MARGIN) - 11
             row = pos[1] // (HEIGHT + MARGIN)
-            if not(bot_turn):
+            if not (bot_turn):
                 output = player.SendHit(row, column, bot)
                 if output[2]:
                     running = False
                     winner = "Player"
-                if not(output[0]) and not(output[1]):
+                if output[0] and not (output[1]):
                     bot_turn = True
-
     # Draw game
     win.fill(WHITE)
-    for index, board in enumerate([player.attack_board.board, player.defense_board.board]):
+    for index, board in enumerate(
+        [player.attack_board.board, player.defense_board.board]
+    ):
         for row, i in enumerate(board):
             for column, case in enumerate(i):
                 offset = 0
@@ -84,15 +88,17 @@ while running:
                     color = RED
                 else:
                     color = GREEN
-                pygame.draw.rect(win,
-                                 color,
-                                 [(MARGIN + WIDTH) * column + MARGIN + offset,
-                                  (MARGIN + HEIGHT) * row + MARGIN,
-                                  WIDTH,
-                                  HEIGHT])
-
-
+                pygame.draw.rect(
+                    win,
+                    color,
+                    [
+                        (MARGIN + WIDTH) * column + MARGIN + offset,
+                        (MARGIN + HEIGHT) * row + MARGIN,
+                        WIDTH,
+                        HEIGHT,
+                    ],
+                )
     pygame.display.flip()
 print(f"{winner} a gagné en {coup} coups")
-imageio.mimsave('heatmap/heatmap.gif', bot.images, fps = 2)
+imageio.mimsave("heatmap/heatmap.gif", bot.images, fps=2)
 pygame.quit()
